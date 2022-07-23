e5fd8be48458a8443fe700cf1301333252400288
Jamie Gaskins <jgaskins@hey.com>
2022-07-22 20:44:32 -0400

    Fix HMSET, deprecate it, and add HSET overloads
    HMSET is deprecated but there isn't much cost to holding onto it for
    now while it's still supported.

--- src/commands/hash.cr
+++ src/commands/hash.cr
@@ -11,11 +11,36 @@ module Redis::Commands::Hash
     run({"hmget", key} + fields)
   end
 
-  def hset(key : String, field : String, value : String)
-    run({"hset", key, field, value})
+  def hset(key : String, **fields : String)
+    hash = ::Hash(String, String).new(initial_capacity: fields.size)
+    fields.each do |key, value|
+      hash[key.to_s] = value
+    end
+
+    hset key, hash
   end
 
-  def hmset(key : String, data : Hash(String, String))
+  def hset(key : String, *fields : String)
+    run({"hset", key} + fields)
+  end
+
+  def hset(key : String, *fields : String)
+    run({"hset", key} + fields)
+  end
+
+  def hset(key : String, data : ::Hash(String, String))
+    command = Array(String).new(initial_capacity: 2 + data.size)
+
+    command << "hset" << key
+    data.each do |key, value|
+      command << key << value
+    end
+
+    run command
+  end
+
+  @[Deprecated("The Redis HMSET command is deprecated. Use HSET instead. This method will be removed in v1.0.0 of this shard. See https://redis.io/commands/hmset/")]
+  def hmset(key : String, data : ::Hash(String, String))
     command = Array(String).new(initial_capacity: 2 + data.size)
 
     command << "hmset" << key
