jgaskins/redis

commit aafe485f21c5b6582aa5ef7a9f979e94713dbd1b

Optimize simple strings for `+OK\r\n` responses
Previously, `redis.set "foo", "bar"` allocated 16 bytes of heap memory
for the "OK" response from the server. With this commit, it doesn't
allocate any heap memory at all.

src/parser.cr
@@ -48,7 +48,22 @@ module Redis
           value
         end
       when '+'
-        @io.read_line
+        # Most of the time, RESP simple strings are just "OK", so we can
+        # optimize for that case to avoid heap allocations. If it is *not* the
+        # "OK" string, this does an extra heap allocation, but that seems like
+        # a decent tradeoff considering the vast majority of times a simple
+        # string will be returned from the server is from a SET call.
+        buffer = uninitialized UInt8[4] # "OK\r\n"
+        slice = buffer.to_slice
+        read = @io.read slice
+        result = if read == 4 && slice == "OK\r\n".to_slice
+          "OK"
+        else
+          String.build do |str|
+            str.write slice[0...read]
+            str << @io.read_line
+          end
+        end
       when '-'
         type, message = @io.read_line.split(' ', 2)
         ERROR_MAP[type].new("#{type} #{message}")

spec/parser_spec.cr
@@ -12,8 +12,13 @@ module Redis
     end
 
     it "reads simple strings" do
-      io = IO::Memory.new("+OK\r\n")
-      Parser.new(io).read.should eq "OK"
+      io = IO::Memory.new("+OK\r\n+QUEUED\r\n+OK\r\n+QUEUED\r\n")
+      parser = Parser.new(io)
+
+      2.times do
+        parser.read.should eq "OK"
+        parser.read.should eq "QUEUED"
+      end
     end
 
     it "reads bulk strings" do

spec/redis_spec.cr
@@ -27,7 +27,7 @@ end
 describe Redis::Client do
   test "can set, get, and delete keys" do
     redis.get(random_key).should eq nil
-    redis.set(key, "hello")
+    redis.set(key, "hello").should eq "OK"
     redis.get(key).should eq "hello"
     redis.del(key).should eq 1
     redis.del(key).should eq 0

commit f02724bdbc1c9ed0fd3830ebeab0f59a971d96ed

Allow simple strings < 2 bytes to be optimized
The optimization in aafe485 broke simple strings that were less than 2
bytes long. This commit fixes that.

src/parser.cr
@@ -55,14 +55,16 @@ module Redis
         # string will be returned from the server is from a SET call.
         buffer = uninitialized UInt8[4] # "OK\r\n"
         slice = buffer.to_slice
-        read = @io.read slice
-        result = if read == 4 && slice == "OK\r\n".to_slice
+        read = @io.read slice[0, 2] # Just trying to check whether we got "OK"
+        if read == 2 && slice[0, 2] == "OK".to_slice && (second_read = @io.read(slice + 2)) && slice == "OK\r\n".to_slice
           "OK"
+        elsif read == 2 && slice[0, 2] == "\r\n".to_slice
+          ""
         else
           String.build do |str|
-            str.write slice[0...read]
+            str.write slice[0...read + (second_read || 0)]
             str << @io.read_line
-          end
+          end.chomp
         end
       when '-'
         type, message = @io.read_line.split(' ', 2)

spec/parser_spec.cr
@@ -12,12 +12,16 @@ module Redis
     end
 
     it "reads simple strings" do
-      io = IO::Memory.new("+OK\r\n+QUEUED\r\n+OK\r\n+QUEUED\r\n")
+      io = IO::Memory.new("+OK\r\n+QUEUED\r\n+.\r\n+\r\n" * 2)
       parser = Parser.new(io)
 
       2.times do
         parser.read.should eq "OK"
         parser.read.should eq "QUEUED"
+        # With the optimization made in aafe485, we need to ensure we can read
+        # simple strings with a size of less than 2 bytes.
+        parser.read.should eq "."
+        parser.read.should eq ""
       end
     end
 