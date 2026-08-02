c90df83605645fc54dc7a69919590ea85f1999f6
Jamie Gaskins <jgaskins@hey.com>
2026-08-01 22:37:38 -0400

    Improve stream commands and structures

--- spec/streaming_spec.cr
+++ spec/streaming_spec.cr
@@ -3,7 +3,9 @@ require "./spec_helper"
 require "../src/client"
 require "../src/streaming"
 
-module Redis::Streaming
+module StreamingSpec
+  include Redis::Streaming
+
   redis = Redis::Client.new
   define_test redis
   test = TestRunner.new(redis)
@@ -60,6 +62,300 @@ module Redis::Streaming
         response.dig("second_stream", 1, "four").should eq "4"
       ensure
         redis.del "first_stream", "second_stream"
+      end
+    end
+
+    describe XInfoStreamResponse do
+      test "parses info about a stream" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "consumer"
+        id = redis.xadd key, "*", fields: {id: "0"}
+        redis.xreadgroup "group", "consumer", streams: {key => ">"}
+
+        info = XInfoStreamResponse.new(redis.xinfo_stream(key))
+
+        info.entries_added.should eq 1 # XADD has only been called once
+        info.groups.should eq 1        # We added a group above
+        info.length.should eq 1
+
+        redis.xreadgroup "group", "consumer", streams: {key => ">"}
+        redis.xgroup_create key, "another group"
+        XInfoStreamResponse.new(redis.xinfo_stream(key)).groups.should eq 2
+      end
+    end
+
+    describe XInfoStreamFullResponse do
+      test "parses extended info about a stream" do
+        results = redis.pipeline do |redis|
+          redis.xgroup_create key, "group", mkstream: true
+          redis.xgroup_create_consumer key, "group", "one"
+          redis.xgroup_create_consumer key, "group", "two"
+          50.times do |i|
+            redis.xadd key, "*", fields: {id: i.to_s}
+          end
+          redis.xreadgroup "group", "one", streams: {key => ">"}, count: 3
+          redis.xreadgroup "group", "two", streams: {key => ">"}, count: 5
+        end
+        from_one = XReadGroupResponse.new(results[-2].as(Array))
+        from_two = XReadGroupResponse.new(results[-1].as(Array))
+        redis.xackdel key, "group", from_two.each_message.first(3).map(&.id).to_a
+
+        info = XInfoStreamFullResponse.new(redis.xinfo_stream_full(key, count: 10))
+
+        info.entries_added.should eq 50
+        info.groups.size.should eq 1
+        info.length.should eq 47
+      end
+    end
+
+    describe XInfoGroupsResponse do
+      test "parses the groups for a stream" do
+        redis.xgroup_create key, "one", mkstream: true
+        redis.xgroup_create key, "two"
+        redis.xgroup_create_consumer key, "one", "consumer"
+        redis.xadd key, "*", fields: {id: "0"}
+        redis.xreadgroup "one", "consumer", streams: {key => ">"}
+
+        info = XInfoGroupsResponse.new(redis.xinfo_groups(key))
+
+        one, two = info.groups
+        one.name.should eq "one"
+        one.consumers.should eq 1
+        one.last_delivered_id.should_not eq "0-0"
+        two.name.should eq "two"
+        two.consumers.should eq 0
+        two.last_delivered_id.should eq "0-0"
+      end
+    end
+
+    describe XInfoConsumersResponse do
+      test "parses the consumers for a group" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "consumer"
+
+        consumer = XInfoConsumersResponse.new(redis.xinfo_consumers(key, "group")).consumers.first
+
+        consumer.name.should eq "consumer"
+        # It could be either value depending on how fast we query it, so we just
+        # check that it's one of them.
+        consumer.idle.should be_in 0, 1
+        consumer.inactive.should eq -1
+        consumer.pending.should eq 0
+      end
+    end
+
+    describe XPendingResponse do
+      test "parses the PEL for a group" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "one"
+        redis.xgroup_create_consumer key, "group", "two"
+        # Four total messages ...
+        first = redis.xadd key, "*", fields: {id: "1"}
+        second = redis.xadd key, "*", fields: {id: "2"}
+        third = redis.xadd key, "*", fields: {id: "3"}
+        fourth = redis.xadd key, "*", fields: {id: "4"}
+        # ... and two different consumers each read some
+        redis.xreadgroup "group", "one", streams: {key => ">"}, count: 1
+        redis.xreadgroup "group", "two", streams: {key => ">"}, count: 2
+
+        pending = XPendingResponse.new(redis.xpending(key, "group"))
+
+        pending.count.should eq 3
+        pending.earliest.should eq first
+        pending.latest.should eq third
+        one, two = pending.data
+        one.consumer.should eq "one"
+        one.pending_count.should eq 1
+        two.consumer.should eq "two"
+        two.pending_count.should eq 2
+      end
+    end
+
+    describe XPendingExtendedResponse do
+      test "parses the PEL for a group" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "one"
+        redis.xgroup_create_consumer key, "group", "two"
+        # Four total messages ...
+        first_id = redis.xadd key, "*", fields: {id: "1"}
+        second_id = redis.xadd key, "*", fields: {id: "2"}
+        third_id = redis.xadd key, "*", fields: {id: "3"}
+        fourth_id = redis.xadd key, "*", fields: {id: "4"}
+        # ... and two different consumers each read some
+        redis.xreadgroup "group", "one", streams: {key => ">"}, count: 1
+        redis.xreadgroup "group", "two", streams: {key => ">"}, count: 2
+
+        pending = XPendingExtendedResponse.new(
+          redis.xpending key, "group",
+            start: "0",
+            end: "+",
+            count: "4",
+        )
+
+        pending.messages.size.should eq 3
+        first, second, third = pending.messages
+        first.consumer.should eq "one"
+        first.id.should eq first_id
+        second.consumer.should eq "two"
+        second.id.should eq second_id
+        third.consumer.should eq "two"
+        third.id.should eq third_id
+      end
+    end
+  end
+
+  describe Redis::Commands::Stream do
+    describe "creating and deleting groups and consumers" do
+      test "creates and deletes a group" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xinfo_groups(key).should_not be_empty
+
+        redis.xgroup_destroy(key, "group")
+        redis.xinfo_groups(key).should be_empty
+      end
+
+      test "creates and deletes consumers" do
+        redis.xgroup_create key, "group", mkstream: true
+
+        redis.xgroup_create_consumer key, "group", "consumer"
+        redis.xinfo_consumers(key, "group").should_not be_empty
+
+        redis.xgroup_del_consumer key, "group", "consumer"
+        redis.xinfo_consumers(key, "group").should be_empty
+      end
+    end
+
+    describe "acknowledging messages in a consumer group" do
+      test "acks a message" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "consumer"
+        id = redis.xadd key, "*", {id: "0"}
+        get_pending = -> do
+          groups = redis.xinfo_groups(key)
+          Redis.to_hash(groups.first.as(Array))["pending"]
+        end
+
+        get_pending.call.should eq 0 # Nothing in the PEL yet
+
+        redis.xreadgroup "group", "consumer", streams: {key => ">"}
+        get_pending.call.should eq 1 # Reading a msg puts it into the PEL
+
+        redis.xack key, "group", [id.not_nil!]
+        get_pending.call.should eq 0 # XACKing a msg removes it from the PEL
+      end
+
+      test "acks and deletes a message in a single call" do
+        redis.xgroup_create key, "group", mkstream: true
+        redis.xgroup_create_consumer key, "group", "consumer"
+        first = redis.xadd key, "*", {id: "0"}
+        second = redis.xadd key, "*", {id: "1"}
+        redis.xreadgroup "group", "consumer", streams: {key => ">"}, count: 2
+
+        # https://redis.io/docs/latest/commands/xackdel/#optional-arguments
+        # Without a DeleteMode (server defaults to KEEPREF)
+        redis.xackdel key, "group", [first.as(String)]
+        # With an explicit DeleteMode
+        redis.xackdel key, "group", :acked, [second.as(String)]
+
+        redis.xlen(key).should eq 0
+      end
+    end
+
+    describe "trimming" do
+      test "trims a stream to an approximate size with MAXLEN" do
+        redis.multi do |redis|
+          200.times do |i|
+            redis.xadd key, "*", {id: i.to_s}
+          end
+        end
+
+        redis.xtrim(key, maxlen: {"~", "90"}).should eq 100
+      end
+
+      test "trims a stream to an approximate minimum id with MINID" do
+        redis.multi do |redis|
+          200.times do |i|
+            i += 1 # Can't use an id of `0`
+            redis.xadd key, i.to_s, {id: i.to_s}
+          end
+        end
+
+        redis.xtrim(key, minid: {"~", "110"}).should eq 100
+      end
+
+      test "accepts a delete mode" do
+        redis.multi do |redis|
+          redis.xgroup_create key, "group", mkstream: true
+          redis.xgroup_create_consumer key, "group", "consumer"
+
+          200.times do |i|
+            i += 1 # Can't use an id of `0`
+            redis.xadd key, i.to_s, {id: i.to_s}
+          end
+
+          redis.xreadgroup "group", "consumer", streams: {key => ">"}, count: 200
+          redis.xack key, "group", Array.new(200) { |i| (i + 1).to_s }
+        end
+
+        redis.xtrim(key, minid: {"~", "100"}, delete_mode: :acked).should be_within 10, of: 100
+
+        redis.xlen(key).should be_within 10, of: 100
+      end
+    end
+
+    if test.server_version >= Version["8.0.0"]
+      test "publishes and reads per-group" do
+        redis.xgroup_create key, "group1", id: "$", mkstream: true
+        redis.xgroup_create_consumer key, "group1", "consumer"
+        redis.xgroup_create key, "group2", id: "$"
+        redis.xgroup_create_consumer key, "group2", "consumer"
+
+        redis.xadd key, "*", fields: {id: "1"}
+
+        redis.xreadgroup("group1", "consumer", streams: {key => ">"}).should_not be_nil
+        redis.xreadgroup("group1", "consumer", streams: {key => ">"}).should be_nil
+
+        redis.xreadgroup("group2", "consumer", streams: {key => ">"}).should_not be_nil
+        redis.xreadgroup("group2", "consumer", streams: {key => ">"}).should be_nil
+      end
+
+      if test.server_version >= Version["8.4.0"]
+        test "reads with a claim time" do
+          redis.xgroup_create key, "group", id: "$", mkstream: true
+          redis.xgroup_create_consumer key, "group", "consumer1"
+          redis.xgroup_create_consumer key, "group", "consumer2"
+
+          redis.xadd key, "*", fields: {id: "1"}
+
+          redis.xreadgroup("group", "consumer1", streams: {key => ">"}).should_not be_nil
+          sleep 1.millisecond
+          redis.xreadgroup("group", "consumer2", streams: {key => ">"}, claim: 1.millisecond).should_not be_nil
+        end
+      end
+
+      if test.server_version >= Version["8.8.0"]
+        test "negatively acknowledges messages" do
+          redis.xgroup_create key, "group", id: "$", mkstream: true
+          redis.xgroup_create_consumer key, "group", "consumer1"
+          redis.xgroup_create_consumer key, "group", "consumer2"
+          redis.xadd key, "*", fields: {foo: "asdf"}
+          response = XReadGroupResponse.hash(
+            redis.xreadgroup("group", "consumer1", streams: {key => ">"}).not_nil!
+          )
+
+          redis.xnack(key, "group", :fail, response[key].map(&.id)).should eq 1
+
+          # Even though we're specifying a 30-second min claim, we get the
+          # message immediately because of the XNACK command above
+          redis
+            .xreadgroup(
+              group: "group",
+              consumer: "consumer2",
+              streams: {key => ">"},
+              claim: 30.seconds,
+            )
+            .should_not be_nil
+        end
       end
     end
   end
--- src/streaming.cr
+++ src/streaming.cr
@@ -7,18 +7,36 @@ module Redis
 
       getter id : String
       getter values : Hash(String, String)
+      getter last_delivered_at : Time
+      getter delivery_count : Int64
 
       def initialize(message : Array)
-        id, values = message
+        if message.size >= 4
+          id, values, last_delivered_at, delivery_count = message
+        else
+          id, values = message
+          last_delivered_at = 0i64
+          delivery_count = 0i64
+        end
         values = values.as(Array)
+
         @id = id.as(String)
         @values = Hash(String, String).new(initial_capacity: values.size // 2)
+        @last_delivered_at = Time.unix_ms(last_delivered_at.as(Int64))
+        @delivery_count = delivery_count.as(Int64)
+
         (0...values.size).step(2) do |index|
           @values[values[index].as(String)] = values[index + 1].as(String)
         end
       end
 
-      def initialize(@id, @values)
+      def initialize(
+        @id,
+        @values,
+        *,
+        @last_delivered_at = Time::UNIX_EPOCH,
+        @delivery_count = 0,
+      )
       end
 
       delegate each, to: @values
@@ -153,23 +171,74 @@ module Redis
 
     # Transform the `XREADGROUP` result into a more friendly object.
     struct XReadGroupResponse
-      getter results
+      struct Result
+        getter messages : Array(Message)
+        getter key : String
+
+        def initialize(@key, @messages)
+        end
+      end
+
+      include Enumerable(Result)
+
+      getter results : Array(Result)
+
+      def self.hash(response : Array(Redis::Value))
+        results = Hash(String, Array(Message)).new(initial_capacity: response.size)
+        response.each do |row|
+          key, data = row.as(Array)
+          messages = data.as(Array).map { |msg| Message.new(msg.as(Array)) }
+          results[key.as(String)] = messages
+        end
+        results
+      end
 
       def initialize(response : Array(Redis::Value))
         @results = Array(Result).new(initial_capacity: response.size)
 
         response.each do |row|
           key, data = row.as(Array)
-          @results << Result.new(key.as(String), data.as(Array))
+          messages = data.as(Array).map { |msg| Message.new(msg.as(Array)) }
+          @results << Result.new(key.as(String), messages)
         end
       end
 
-      struct Result
-        getter messages : Array(Message)
-        getter key : String
+      delegate each, to: results
 
-        def initialize(@key : String, messages : Array)
-          @messages = messages.map { |msg| Message.new(msg.as(Array)) }
+      def each_message
+        MessageIterator.new(results)
+      end
+
+      def each_message(&)
+        results.each do |result|
+          result.messages.each do |message|
+            yield message
+          end
+        end
+      end
+
+      class MessageIterator
+        include Iterator(Message)
+
+        @results : Array(Result)
+
+        def initialize(@results)
+          @result_index = 0
+          @message_index = -1
+        end
+
+        def next : Message | Stop
+          if result = @results[@result_index]?
+            if message = result.messages[@message_index += 1]?
+              message
+            else
+              @message_index = -1
+              @result_index += 1
+              self.next
+            end
+          else
+            stop
+          end
         end
       end
     end
@@ -186,6 +255,291 @@ module Redis
         @messages = messages.map do |message_data|
           Message.new(message_data.as(Array))
         end
+      end
+    end
+
+    # The `Properties.define` macro is intended to make defining some of the
+    # structs returned as a Redis hash (array in RESP2) easier.
+    #
+    # ```
+    # struct Thing
+    #   Properties.define(
+    #     one : String,
+    #     two : Int64,
+    #   )
+    # end
+    #
+    # Thing.new(["one", "value goes here", "two", 2])
+    # # => Thing(@one="value goes here", @two=2)
+    # ```
+    private module Properties
+      macro define(*properties)
+
+        include Properties
+
+        {% for prop in properties %}
+          getter {{prop}}
+        {% end %}
+
+        def initialize(response : Array)
+          {% for prop in properties %}
+            {{prop.var}} : {{prop.type}}? = nil
+          {% end %}
+
+          response.each_slice 2, reuse: true do |(attribute, value)|
+            case attribute
+            {% for prop in properties %}
+            when {{prop.var.stringify.gsub(/_/, "-")}} then {{prop.var}} = get(value, {{prop.type}})
+            {% end %}
+            end
+          end
+
+          {% for prop in properties %}
+            if {{prop.var}}
+              @{{prop.var}} = {{prop.var}}
+            {% if !prop.type.is_a?(Union) || (prop.type.is_a?(Union) && !prop.type.types.any? { |type| type.names.any? { |name| name.stringify == "Nil" } }) %}
+            else
+              raise NilAssertionError.new("{{prop.var}} must be {{prop.type}}, but was either not provided by the Redis server or was sent as nil ({{prop.type}} - {{prop.type.class_name.id}})")
+            {% end %}
+            end
+          {% end %}
+        end
+      end
+
+      protected def get(value : Int64, type : Int64.class) : Int64
+        value
+      end
+
+      protected def get(value : Value, type : Int64.class) : Int64
+        raise TypeCastError.new("Expected Int64, got: #{value.inspect}")
+      end
+
+      protected def get(value : String, type : String.class) : String
+        value
+      end
+
+      protected def get(value : Value, type : String.class) : String
+        raise TypeCastError.new("Expected String, got: #{value.inspect}")
+      end
+
+      protected def get(value : String?, type : String?.class) : String?
+        value
+      end
+
+      protected def get(value : Value, type : String?.class) : String?
+        raise TypeCastError.new("Expected String?, got: #{value.inspect}")
+      end
+
+      protected def get(value : Int64?, type : Int64?.class) : Int64?
+        value
+      end
+
+      protected def get(value : Value, type : Int64?.class) : Int64?
+        raise TypeCastError.new("Expected Int64?, got: #{value.inspect}")
+      end
+
+      protected def get(value : Array(Value), type : Array(Value).class)
+        value.as Array(Value)
+      end
+
+      protected def get(value : Value, type : Time.class) : Time
+        Time.unix_ms value.as(Int64)
+      end
+    end
+
+    @[Experimental]
+    struct XInfoStreamResponse
+      Properties.define(
+        length : Int64,
+        radix_tree_keys : Int64,
+        radix_tree_nodes : Int64,
+        last_generated_id : String,
+        max_deleted_entry_id : String,
+        idmp_duration : Int64,
+        idmp_maxsize : Int64,
+        entries_added : Int64,
+        recorded_first_entry_id : String,
+        pids_tracked : Int64,
+        iids_tracked : Int64,
+        iids_added : Int64,
+        iids_duplicates : Int64,
+        groups : Int64,
+        first_entry : Message?,
+        last_entry : Message?,
+      )
+
+      protected def get(value : Redis::Value, type : Message?.class)
+        Message.new(value.as(Array))
+      end
+    end
+
+    @[Experimental]
+    struct XInfoStreamFullResponse
+      Properties.define(
+        length : Int64,
+        radix_tree_keys : Int64,
+        radix_tree_nodes : Int64,
+        last_generated_id : String,
+        max_deleted_entry_id : String,
+        entries_added : Int64,
+        recorded_first_entry_id : String,
+        idmp_duration : Int64,
+        idmp_maxsize : Int64,
+        pids_tracked : Int64,
+        iids_tracked : Int64,
+        iids_added : Int64,
+        iids_duplicates : Int64,
+        entries : Array(Message),
+        groups : Array(Group),
+      )
+      [
+        "length", 1,
+        "radix-tree-keys", 1,
+        "radix-tree-nodes", 2,
+        "last-generated-id", "1780361760536-0",
+        "max-deleted-entry-id", "0-0",
+        "entries-added", 1,
+        "recorded-first-entry-id", "1780361760536-0",
+        "idmp-duration", 100,
+        "idmp-maxsize", 100,
+        "pids-tracked", 0,
+        "iids-tracked", 0,
+        "iids-added", 0,
+        "iids-duplicates", 0,
+        "entries", [["1780361760536-0", ["id", "0"]]],
+        "groups", [
+          [
+            "name", "group",
+            "last-delivered-id", "1780361760536-0",
+            "entries-read", 1,
+            "lag", 0,
+            "pel-count", 1,
+            "nacked-count", 0,
+            "pending", [["1780361760536-0", "consumer", 1780361760536, 1]],
+            "consumers", [
+              [
+                "name", "consumer",
+                "seen-time", 1780361760536,
+                "active-time", 1780361760536,
+                "pel-count", 1,
+                "pending", [["1780361760536-0", 1780361760536, 1]],
+              ],
+            ],
+          ],
+        ],
+      ]
+
+      protected def get(value : Redis::Value, type : Array(Message).class) : Array(Message)
+        value.as(Array).map { |data| Message.new(data.as(Array)) }
+      end
+
+      protected def get(value : Redis::Value, type : Array(Group).class) : Array(Group)
+        value.as(Array).map { |data| Group.new(data.as(Array)) }
+      end
+
+      struct Group
+        Properties.define(
+          name : String,
+          last_delivered_id : String?,
+          entries_read : Int64?,
+          lag : Int64,
+          pel_count : Int64,
+          nacked_count : Int64,
+          pending : Array(PendingEntry),
+          consumers : Array(Consumer),
+        )
+
+        protected def get(value : Redis::Value, type : Array(PendingEntry).class) : Array(PendingEntry)
+          value.as(Array).map do |data|
+            id, consumer, last_delivered_at, delivery_count = data.as(Array)
+
+            PendingEntry.new(
+              id: id.as(String),
+              consumer: consumer.as(String),
+              last_delivered_at: Time.unix_ms(last_delivered_at.as(Int64)),
+              delivery_count: delivery_count.as(Int64),
+            )
+          end
+        end
+
+        protected def get(value : Redis::Value, type : Array(Consumer).class) : Array(Consumer)
+          value.as(Array).map { |data| Consumer.new(data.as(Array)) }
+        end
+
+        struct PendingEntry
+          getter id : String
+          getter consumer : String
+          getter last_delivered_at : Time
+          getter delivery_count : Int64
+
+          def initialize(@id, @consumer, @last_delivered_at, @delivery_count)
+          end
+        end
+      end
+
+      struct Consumer
+        Properties.define(
+          name : String,
+          seen_time : Time,
+          active_time : Time,
+          pel_count : Int64,
+          pending : Array(PendingEntry),
+        )
+
+        protected def get(value : Redis::Value, type : Array(PendingEntry).class) : Array(PendingEntry)
+          value.as(Array).map do |entry|
+            data = entry.as(Array)
+            PendingEntry.new(data[0].as(String), data[1].as(Int64), data[2].as(Int64))
+          end
+        end
+
+        struct PendingEntry
+          getter id : String
+          getter last_delivered_at : Time
+          getter delivery_count : Int64
+
+          def initialize(@id, last_delivered_at : Int64, @delivery_count)
+            @last_delivered_at = Time.unix_ms last_delivered_at
+          end
+        end
+      end
+    end
+
+    @[Experimental]
+    struct XInfoGroupsResponse
+      getter groups : Array(Group)
+
+      def initialize(response : Array)
+        @groups = response.map { |data| Group.new data.as(Array) }
+      end
+
+      struct Group
+        Properties.define(
+          name : String,
+          consumers : Int64,
+          pending : Int64,
+          last_delivered_id : String?,
+          entries_read : Int64?,
+          lag : Int64,
+        )
+      end
+    end
+
+    @[Experimental]
+    struct XInfoConsumersResponse
+      getter consumers : Array(Consumer)
+
+      def initialize(response : Array)
+        @consumers = response.map { |data| Consumer.new data.as(Array) }
+      end
+
+      struct Consumer
+        Properties.define(
+          name : String,
+          pending : Int64,
+          idle : Int64,
+          inactive : Int64,
+        )
       end
     end
   end
